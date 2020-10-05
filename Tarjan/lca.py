54195c32ae07e0c63d6162ee4c3478d45291846e
Idan <55255630+1d4n@users.noreply.github.com>
2020-10-05 12:33:17 +0300

    Add files via upload

---
+++ Tarjan/lca.py
@@ -0,0 +1,39 @@
+import json
+
+
+def lca(pair):
+    """
+    Return the Lowest-Common-Anccestor of 2 nodes in a binary tree
+    Args:
+        a pair of 2 indexes in the tree [i, j]
+    """
+    high = max(pair)
+    low = min(pair)
+    equal = high == low
+    
+    if not equal:
+        if high % 2 == 0:
+            high = (high - 2) // 2
+            pair = [high, low]
+        else:
+            high = (high - 1) // 2
+            pair = [high, low]
+    else:
+        return pair[0]
+    
+    return lca(pair)
+
+
+if __name__ == '__main__':
+    with open('tree.txt', 'r') as tree_file:
+        tree = tree_file.read()
+
+    with open('pairs.txt', 'r') as pairs_file:
+        pairs_list = json.load(pairs_file)
+
+    flag = ''.join([tree[lca(pair)] for pair in pairs_list])
+    print("The flag is:", flag)
+
+
+
+
---
+++ Tricky Guess/tricky.py
@@ -0,0 +1,60 @@
+import socket
+import time
+
+
+def init_conn(conn, host, port):
+    conn.connect((host, port))
+    conn.recv(1024)
+    print(conn.recv(8).decode())  # GO!
+
+
+def get_words():
+    with open('words.txt', 'r') as w:
+        w_list = w.readlines()
+    return w_list
+
+
+def common_letters(words_list, current_word, number):
+    filtered_list = list()
+    for word in words_list:
+        counter = 0
+        for char in current_word:
+            if char in word:
+                counter += 1
+            if counter > number:
+                filtered_list.append(word)
+                break
+    return filtered_list
+
+
+if __name__ == '__main__':
+    found = False
+    attempts = 0
+    start_time = time.time()
+    
+    while not found:
+        print('---------------------------')
+        print("Connecting to the server...")
+        attempts += 1
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            init_conn(s, 'tricky-guess.csa-challenge.com', 2222)
+            
+            words = get_words()
+            for i in range(1, 16):
+                print(f"#{i}: ({len(words)} possible words)")
+                word_to_send = words.pop()
+                print("Sending the word:", word_to_send, end="")
+                s.send(word_to_send.encode() + b'\n')
+                
+                respond = s.recv(64).decode()
+                if len(respond) > 3:
+                    print("The flag is:", respond)
+                    found = True
+                    break
+                
+                print("Common letters:", respond)
+                words = common_letters(words, word_to_send, int(respond))
+        
+        print("Total attempts:", attempts)
+        print("Total time:", round(time.time() - start_time), "seconds.")
