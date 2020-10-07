3c22fe8241e4e2b96d24f22b36edaa721acd3280
Idan <55255630+1d4n@users.noreply.github.com>
2020-10-07 11:17:38 +0300

    Update seti.py

--- SETI/seti.py
+++ SETI/seti.py
@@ -33,18 +33,18 @@ def is_char_fit(byte, bits_list):
     return True
 
 
-first_options = list()
+first_options = dict()
 for i in range(len(first_message)):
-    first_options.append([chr(j) for j in range(32, 126)
-                          if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), first_message[i])])
+    first_options[i] = [chr(j) for j in range(32, 126)
+                        if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), first_message[i])]
 print("options for first message:", first_options)
 
-second_options = list()
+second_options = dict()
 for i in range(len(second_message)):
-    second_options.append([chr(j) for j in range(32, 126)
-                          if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), second_message[i])])
+    second_options[i] = [chr(j) for j in range(32, 126)
+                         if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), second_message[i])]
 print("\noptions for second message:", second_options)
-
+print("CSA{L1ttL3_P30pL3,_WhY_K4'Nt_w3_4LL_Ju5t_93t_4L0N9?}")
 #############################################################
 #       Little People, Why Can't We All Just Get Along?     #
 #############################################################
