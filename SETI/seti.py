b1e16d2f92a598d2ef263eb362a2c3bd902d6705
Idan <55255630+1d4n@users.noreply.github.com>
2020-10-11 15:19:41 +0300

    Update seti.py

--- SETI/seti.py
+++ SETI/seti.py
@@ -1,21 +1,19 @@
 import json
 
 with open('first_signal.txt', 'r') as first_file:
-    first_message = json.load(first_file)
+    first_signal = json.load(first_file)
 with open('second_signal.txt', 'r') as second_file:
-    second_message = json.load(second_file)
+    second_signal = json.load(second_file)
 
-for i in range(len(first_message)):
-    first_message[i] = [''.join(map(str, first_message[i][j])) for j in range(len(first_message[i]))]
-for i in range(len(second_message)):
-    second_message[i] = [''.join(map(str, second_message[i][j])) for j in range(len(second_message[i]))]
+first_message = [[''.join(map(str, sub)) for sub in sub_list] for sub_list in first_signal]
+second_message = [[''.join(map(str, sub)) for sub in sub_list] for sub_list in second_signal]
 
 
 def is_fit(byte, bits):
     b = 0
     for bit in bits:
         found = False
-        while b < 8:
+        while b < len(byte):
             if byte[b] == bit:
                 b += 1
                 found = True
@@ -33,18 +31,13 @@ def is_char_fit(byte, bits_list):
     return True
 
 
-first_options = dict()
-for i in range(len(first_message)):
-    first_options[i] = [chr(j) for j in range(32, 126)
-                        if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), first_message[i])]
-print("options for first message:", first_options)
+first_options = [[chr(j) for j in range(32, 126) if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), char)] for char in first_message]
+print("options for first message:\n", first_options)
 
-second_options = dict()
-for i in range(len(second_message)):
-    second_options[i] = [chr(j) for j in range(32, 126)
-                         if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), second_message[i])]
-print("\noptions for second message:", second_options)
-print("CSA{L1ttL3_P30pL3,_WhY_K4'Nt_w3_4LL_Ju5t_93t_4L0N9?}")
+second_options = [[chr(j) for j in range(32, 126) if chr(j) not in f"/[]^*()=;><+" and is_char_fit(bin(j)[2:].zfill(8), char)] for char in second_message]
+print("\noptions for first message:\n", second_options)
+
+print("\nFLAG: CSA{L1ttL3_P30pL3,_WhY_K4'Nt_w3_4LL_Ju5t_93t_4L0N9?}")
 #############################################################
 #       Little People, Why Can't We All Just Get Along?     #
 #############################################################
