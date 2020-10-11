bcd94e2c7be4ed8657e25ba56384aebaf42cd74c
Idan <55255630+1d4n@users.noreply.github.com>
2020-10-11 14:41:30 +0300

    Update cs_hacked.py

--- CS Hacked/cs_hacked.py
+++ CS Hacked/cs_hacked.py
@@ -1,57 +1,47 @@
 import socket
 import time
 from Crypto.Cipher import ARC4
-from itertools import product
+from itertools import product, groupby
 
 
-def get_perms():
+def sequence_perms():
     with open('dictionary.txt', 'r') as d:
-        words_len = dict()
-        for w in d.read().split('\n'):
-            if len(w) not in words_len.keys():
-                words_len[len(w)] = [w]
-                continue
-            words_len[len(w)].append(w)
+        words = [word for word in d.read().split('\n')]
     with open('packets.txt', 'r') as p:
-        options = list()
-        for packet in p.read().split('\n'):
-            options.append((words_len[len(bytes.fromhex(packet)) - 1]))
-    poss_perm = list()
-    for o in product(*options):
-        poss_perm.append(o)
-    return poss_perm
+        packets_len = [(len(packet) // 2 - 1) for packet in p.read().split('\n')]
+    
+    words.sort(key = len)
+    words_len = {length: list(set(items)) for length, items in groupby(words, key=len) if length in packets_len}
+    possible_words = [words_len[packet_len] for packet_len in packets_len]
+    print(possible_words)
+
+    return [o for o in product(*possible_words)]
 
 
-perms = get_perms()
+perms = sequence_perms()
 start_time = time.time()
+# The correct sequence is: 'particularly', 'administration', 'a', 'as', 'I', 'environmental', 'about', 'across', 'ability', 'according'
 
-# the correct sequence is in perms[37] 
-for i in range(0, len(perms)):
-    attempt_time = time.time()
+for i in range(len(perms)):
     with socket.socket() as conn:
-        conn.connect(('3.126.154.76', 80))
-        key = conn.recv(128).decode()
-        print(key, end="")
-        key = (key[key.find(':') + 2: -1]).encode()  # csa-mitm-key
-        cipher = ARC4.new(key)
-        message = conn.recv(128)
-        print(cipher.decrypt(message).decode(), end="")
-
-        
-        for j in range(10):
-            word = perms[i][j] + '\n'
-            print(word, end='')
-            enc_word = cipher.encrypt(word.encode())
-            conn.send(enc_word)
-        print("please wait...")
-        resp = conn.recv(128)
-        flag = cipher.decrypt(resp).decode()
-        print(flag)
-
-        print("----------------------------------")
-        print("Total attempts", i + 1)
-        print("Time for this attempt:", time.time() - attempt_time, "seconds")
+        print(f"--------------- {i + 1} ----------------")
         print("Total time:", round(time.time() - start_time), "seconds.")
-        print("----------------------------------\n")
-        if 'CSA' in flag:
+        conn.connect(('3.126.154.76', 80))
+        
+        msg = conn.recv(64).decode()
+        print(msg[:-1])  # Welcome! your RC4 key is: csa-mitm-key
+        key = (msg[msg.find('csa'):-1]).encode()  # csa-mitm-key
+        cipher = ARC4.new(key)
+        print(cipher.decrypt(conn.recv(32)).decode(), end="")  # Hi! what's the secret sequence?
+        
+        sequence = ' '.join(perms[i])
+        print(sequence)
+        conn.send(cipher.encrypt(sequence.encode()))
+        print("please wait...")
+        
+        flag = cipher.decrypt(conn.recv(128))
+        if b'CSA' in flag:
+            print("The flag is:", flag.decode())
             break
+        else:
+            print("Wrong sequence - reconnecting...\n")
